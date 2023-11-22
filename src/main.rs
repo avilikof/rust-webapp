d0e388f9d64dd9fe0e56d60bf1e27f5e09dfcdb3
Alex <alex@Aleksandrs-MacBook-Pro-2.local>
2023-11-22 23:31:43 +0200

    feat: add rocker framework

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,13 @@
-fn main() {
-    println!("Hello, world!");
-    loop {}
+#[macro_use]
+extern crate rocket;
+
+#[get("/")]
+fn index() -> &'static str {
+    "Hello, world!"
 }
+
+#[launch]
+fn rocket() -> _ {
+    rocket::build().mount("/", routes![index])
+}
+
