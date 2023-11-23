db0531a9773fe8962e6d851d576e253a74c6333f
Alex <alex@Aleksandrs-MacBook-Pro-2.local>
2023-11-23 12:15:46 +0200

    fix: typo in cargo.toml fixed

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,14 @@
-use std::fmt::format;
-
+use std::env;
 use axum::{routing::get, Router};
+
 
 #[tokio::main]
 async fn main() {
     // build our application with a single route
     let app = Router::new().route("/", get(|| async { "Hello, World!" }));
 
-    let port = std::env("PORT");
     // run it with hyper on localhost:3000
-    axum::Server::bind(&format!("0.0.0.0:{}", port).parse().unwrap())
+    axum::Server::bind(&"0.0.0.0:8080".parse().unwrap())
         .serve(app.into_make_service())
         .await
         .unwrap();
