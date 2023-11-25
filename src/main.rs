a680ccb1de05dadb05ac686d4469896951373323
Alex <alex@Aleksandrs-MacBook-Pro-2.local>
2023-11-25 14:21:47 +0200

    feat: response with exec duration

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,17 @@
-use axum::{routing::get, Router};
-use log::{debug, info, LevelFilter};
+use axum::{routing::get, Json, Router};
+use log::{debug, LevelFilter};
 
-use std::env;
+use serde::Serialize;
+use std::{
+    env,
+    time::{Duration, Instant},
+};
 
+#[derive(Serialize)]
+struct ExecTime {
+    response: String,
+    time: Duration,
+}
 
 #[tokio::main]
 async fn main() {
@@ -20,16 +29,46 @@ async fn main() {
         .unwrap();
 }
 
-async fn text_from_var() -> String {
+async fn text_from_var() -> Json<ExecTime> {
     debug!("/text route executed");
-    match env::var("PORT") {
-        Err(e) => e.to_string(),
-        Ok(var) => var
-    }
+    // match env::var("PORT") {
+    //     Err(e) => e.to_string(),
+    //     Ok(var) => var,
+    // }
+    let start_time = Instant::now();
+    let env_var = get_env("PORT");
+    let end_time = Instant::now();
+    let duration = end_time - start_time;
+    Json(execution_report(duration, &env_var))
 }
 
 fn init_logging() {
     env_logger::builder()
         .filter_level(LevelFilter::Debug) // Adjust log level as needed
         .init();
+}
+
+fn execution_time<F, S>(func: F) -> (Duration, String)
+where
+    F: FnOnce() -> std::string::String,
+{
+    let start_time = Instant::now();
+    let func_resp: String = func();
+    let end_time = Instant::now();
+    (end_time - start_time, func_resp)
+}
+
+fn get_env(env_name: &str) -> String {
+    let env_name_caps = env_name.to_owned().to_uppercase();
+    match env::var(env_name_caps) {
+        Err(e) => e.to_string(),
+        Ok(syst_var) => syst_var,
+    }
+}
+
+fn execution_report(duration: Duration, result: &str) -> ExecTime {
+    ExecTime {
+        time: duration,
+        response: result.to_owned(),
+    }
 }
