7541bd05e175db6046df8a4dbd2ec2e511ea3d92
Alex <alex@Aleksandrs-MacBook-Pro-2.local>
2023-11-25 22:22:19 +0200

    fix: fix group_id var

--- src/libs/settings.rs
+++ src/libs/settings.rs
@@ -1,4 +1,5 @@
 use std::env;
+use log::error;
 
 #[derive(Debug, Clone)]
 pub struct  StreamingSettings {
@@ -11,8 +12,9 @@ pub struct  StreamingSettings {
 
 fn get_env(env_name: &str) -> String {
     let env_name_caps = env_name.to_owned().to_uppercase();
-    match env::var(env_name_caps) {
+    match env::var(&env_name_caps) {
         Err(_) => {
+            error!("Cannot get system variable: {}", &env_name_caps);
             panic!("failed to get settings from variables");
         },
         Ok(sys_var) => sys_var
@@ -25,7 +27,7 @@ impl StreamingSettings {
         let username = get_env("UPSTASH_KAFKA_REST_USERNAME");
         let password = get_env("UPSTASH_KAFKA_REST_PASSWORD");
         let default_topic = get_env("UPSTASH_KAFKA_WRITE_TOPIC");
-        let group_id = get_env("Fl0Client");
+        let group_id = get_env("UPSTASH_KAFKA_GROUP_ID");
         StreamingSettings{
             bootstrap,
             username,
